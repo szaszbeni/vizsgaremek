d930e2e7e81fee4f497ac4562a04be76ff42e8f9
szaszbeni <71766125+szaszbeni@users.noreply.github.com>
2024-12-09 14:16:04 +0100

    Add files via upload

---
+++ molnarzsombor/kapcsolat.php
@@ -0,0 +1,11 @@
+<?php
+    $adb = mysqli_connect("localhost", "root", "", "hurkak");
+
+    function randomstring($h)
+    {
+	$c = "0123456789abcdefghijklmnopqrstuvwxyz" ;
+	$s = "" ;
+	for( $i=0; $i<$h; $i++ )  $s .= substr( $c , rand(0,strlen($c)-1) , 1 ) ;
+	return $s ;
+    }
+?>
