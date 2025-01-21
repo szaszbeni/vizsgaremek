8ca4ee76432089f33a0971bd6e534372ab5a54a1
szaszbeni <71766125+szaszbeni@users.noreply.github.com>
2025-01-21 16:15:56 +0100

    Add files via upload

---
+++ vizsgaremek/uzenet_belso.php
@@ -0,0 +1,23 @@
+<?php
+
+    print_r( $_POST ) ;
+    print "<hr>" ;
+    print_r( $_FILES ) ;
+
+    $fajlnev = "uzenet.txt" ;
+    if( !file_exists($fajlnev) )
+    {
+	$fp = fopen( $fajlnev , "w" ) ;
+	fclose( $fp ) ;
+    }
+
+ //   if( mb_strlen($_POST['uzi'])<1 )  print( "<script>  alert('$_POST[uzi] : Írj legalább 10 karaktert!') </script>" );
+
+    $kep = $_FILES['fajl'] ;
+    $sz  = date("Y-m-d H:i:s") . ";" . ";" . str_replace( "\r\n" , "<br>" , $_POST['uzi'] ) . "\r\n"  ;
+
+    $fp = fopen( $fajlnev , "a" ) ; 
+    fwrite( $fp , $sz ) ;
+    fclose( $fp ) ;
+
+?>
