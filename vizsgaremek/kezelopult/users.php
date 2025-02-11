b0cc46f40738ca1a3b4a22097089b68d638ed50f
szaszbeni <71766125+szaszbeni@users.noreply.github.com>
2025-02-11 13:59:08 +0100

    Add files via upload

---
+++ vizsgaremek/kezelopult/users.php
@@ -0,0 +1,83 @@
+<?php
+$adb = mysqli_connect( "localhost", "root", "", "szb_registration" );
+
+$query = "SELECT * FROM user";
+$result = mysqli_query($adb, $query);
+
+if (!$result) {
+    echo "Hiba történt az adatok lekérdezésekor.";
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vizsgaremek/vizsga.css">
+    <title>Felhasználók</title>
+    <style>
+       body{
+        background-image: url("../kepek/hatter3.jpeg");
+       }
+       h1{
+        color: white;
+       }
+        table {
+            background-color: #f2f2f2;
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+        th, td {
+            padding: 12px;
+            text-align: center;
+        }
+        th {
+            background-color: #f2f2f2;
+            font-weight: bold;
+        }
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+        tr:hover {
+            background-color: #ddd;
+        }
+        h1{
+            text-align: center ;
+        }
+    </style>
+</head>
+<body>
+    <h1>Felhasználók listája</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Registration time</th>
+                
+                
+                
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . htmlspecialchars($row['uid']) . "</td>";
+                echo "<td><a href='adatlap_form1.php?uid=" . urlencode($row['uid']) . "'>" . htmlspecialchars($row['uname']) . "</a></td>";
+                echo "<td>" . htmlspecialchars($row['uemail']) . "</td>";
+                echo "<td>" . htmlspecialchars($row['utime']) . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
